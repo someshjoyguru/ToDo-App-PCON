3b2aed20ae4b88bb348949fbf5ec1b4766d2e85f
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2023-12-24 23:00:41 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,6 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
