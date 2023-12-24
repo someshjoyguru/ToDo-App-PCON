9598188459223a0465869682ecfffb0cc411eec6
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2023-12-24 23:30:17 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: 'http://localhost:5173',
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
