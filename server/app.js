ab2cf3bb380daa2f221ff7e2488c8942484e42bf
Somesh Ghosh <99559848+someshjoyguru@users.noreply.github.com>
2023-12-26 20:27:33 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://achieveit-mocha.vercel.app'],
+    origin: ['http://localhost:5173', 'https://achieveit-mocha.vercel.app', 'https://achieve-it-omega.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
